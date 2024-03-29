8bb217f1f8071864cddd5daa460e252dfbc25fe9
Rafael Guibson <rafaelguibsons@gmail.com>
2024-03-29 13:12:43 -0300

    Adicionado Projeto SpringBoot

---
+++ src/main/java/com/progweb/progweb/ProgwebApplication.java
@@ -0,0 +1,13 @@
+package com.progweb.progweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progweb/progweb/ProgwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progweb.progweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
