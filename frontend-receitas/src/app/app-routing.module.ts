f58e67a86f8dd1b2652be94818df0247d53fbeaf
rafaelguibson <rafaelguibsons@gmail.com>
2024-05-10 18:01:21 -0300

    Adicionado rota para dataTable

--- frontend-receitas/src/app/app-routing.module.ts
+++ frontend-receitas/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DatatableComponent} from "./component/datatable/datatable.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DatatableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-receitas/src/app/app.module.ts
+++ frontend-receitas/src/app/app.module.ts
@@ -4,14 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './menu/navbar/navbar.component';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import { DatatableComponent } from './component/datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbar,
+    MatButton,
+    MatIcon,
+    MatIconButton
   ],
   providers: [
     provideAnimationsAsync()
