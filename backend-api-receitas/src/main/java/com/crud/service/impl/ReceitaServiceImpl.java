c14dba4b6dc2b91eb210fab88faac37767086666
rafaelguibson <rafaelguibsons@gmail.com>
2024-05-17 17:57:44 -0300

    Adicionada duas validacoes de inserir

--- backend-api-receitas/src/main/java/com/crud/service/impl/ReceitaServiceImpl.java
+++ backend-api-receitas/src/main/java/com/crud/service/impl/ReceitaServiceImpl.java
@@ -1,11 +1,15 @@
 package com.crud.service.impl;
 
 import com.crud.exception.MandatoryException;
+import com.crud.exception.RendimentoInvalidoException;
+import com.crud.exception.TempoPreparoInvalidoException;
 import com.crud.model.Receita;
 import com.crud.repository.ReceitaRepository;
 import com.crud.service.ReceitaService;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class ReceitaServiceImpl extends GenericCrudService<Receita, Long, ReceitaRepository> implements ReceitaService {
@@ -19,7 +23,12 @@ public class ReceitaServiceImpl extends GenericCrudService<Receita, Long, Receit
 
     @Override
     protected void validateBusinessLogicForInsert(Receita dado) {
-
+        if(dado.getTempoPreparo() <= 0) {
+            throw new TempoPreparoInvalidoException();
+        }
+        if(Objects.isNull(dado.getRendimento()) ||dado.getRendimento().isEmpty() || dado.getRendimento().isBlank()) {
+            throw new RendimentoInvalidoException();
+        }
     }
 
     @Override
