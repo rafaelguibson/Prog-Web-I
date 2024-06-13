7410a598ffdcea7f73937724494d52ba35b16301
Rafael Guibson <rafaelguibsons@gmail.com>
2024-06-13 12:23:34 -0300

    Adicionado CustomUserDetailsService.

---
+++ backend-api-consultas/src/main/java/com/crud/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.crud.security;
+
+import com.crud.model.User;
+import com.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found."));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
