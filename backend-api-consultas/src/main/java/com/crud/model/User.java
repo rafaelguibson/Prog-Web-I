bafcf58aaf2d4a66378d7020713cd43d0f5b92d0
Rafael Guibson <rafaelguibsons@gmail.com>
2024-06-13 11:26:31 -0300

    Adicionado a entidade usuário.

---
+++ backend-api-consultas/src/main/java/com/crud/model/User.java
@@ -0,0 +1,29 @@
+package com.crud.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+}
